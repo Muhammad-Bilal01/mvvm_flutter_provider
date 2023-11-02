28e7cc93b879326fe265c75ff33c2130c16a62d2
Muhammad Bilal <mbilal254.m@gmail.com>
2023-11-02 23:41:48 +0500

    add comments on app_exceptions.dart

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -1,16 +1,22 @@
+// AppException is a custom exception class that extends the built-in Exception class in Dart.
 class AppException implements Exception {
   final String? _message;
   final String? _prefix;
 
   AppException(this._message, this._prefix);
 
+// overridig the to String method
   @override
   String toString() {
     return "$_prefix $_message";
   }
 }
 
+// FetchDataException is a subclass of AppException.
+// It's specifically designed to handle exceptions related to data fetching or communication errors.
 class FetchDataException extends AppException {
+  // The super keyword is used to call the constructor of the superclass (AppException)
+  // and pass the error message and a fixed prefix, "Error During Communication."
   FetchDataException([String? message])
       : super(message, "Error During Communication");
 }
